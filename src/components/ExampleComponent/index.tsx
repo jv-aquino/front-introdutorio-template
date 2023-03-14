da605f3efe0eb6391aca7c7feddb920c63dca230
TiagoMLucio <tiagolucio100@gmail.com>
2023-03-14 09:10:56 -0300

    :rocket: first commit

---
+++ src/components/ExampleComponent/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './styles';
+
+export const ExampleComponent: React.FC = () => {
+    return <S.Logo src="/assets/images/logo.png" width="128px" height="auto" />;
+};
+
+export default ExampleComponent;
